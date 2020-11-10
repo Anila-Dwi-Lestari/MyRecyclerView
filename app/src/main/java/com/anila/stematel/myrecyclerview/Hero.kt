5b7ee4abcfae52bdc9bfec669d90c0faac2e48d3
Anila-Dwi-Lestari <aniladwilestariii@gmail.com>
2020-11-10 10:12:41 +0700

    first commit

---
+++ app/src/main/java/com/anila/stematel/myrecyclerview/Hero.kt
@@ -0,0 +1,11 @@
+package com.anila.stematel.myrecyclerview
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
